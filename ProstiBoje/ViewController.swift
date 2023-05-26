d09f1f13aeb8d83db283ccce0a399656d4c49a8d
DevMargarita <123300530+DevDaisy1@users.noreply.github.com>
2023-05-26 16:19:29 +0700

    Add files via upload

---
+++ ProstiBoje/ViewController.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+    
+    // MARK: – Actions
+    
+    
+  
+    
+}
+
+
+
---
+++ ProstiBoje/playgraund.swift
@@ -0,0 +1,19 @@
+//
+//  playgraund.swift
+//  ProstiBoje
+//
+//  Created by student on 20.05.2023.
+//
+
+import UIKit
+
+class playgraund: NSObject {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // Обработка действия кнопки
+    }
+
+}
